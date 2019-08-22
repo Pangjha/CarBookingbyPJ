d8a6183bf4b7c7afcf9d78bf8ad3ac4152cc21d0
Peerapat Worapipat <pee1159@hotmail.com>
2019-08-22 11:45:49 +0700

    STEP5

--- app/src/main/java/com/android/example/carlab59160384/MainActivity.kt
+++ app/src/main/java/com/android/example/carlab59160384/MainActivity.kt
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
-import android.widget.ImageView
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var up : Button = findViewById(R.id.update_but);
+    var del : Button = findViewById(R.id.delete_but);
 
     override fun onCreate(savedInstanceState: Bundle?) {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
